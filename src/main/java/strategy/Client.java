fe0a9a0149fbf666e65442aa84ddf3265947300e
sunghyunkang <ksh901016@naver.com>
2018-07-19 19:44:04 +0900

    strategy 전략패턴 - 알고리즘의 세부 구현을 변경할 필요 없이 쉽게 교환할 수 있게 해주는 디자인 패턴

---
+++ src/main/java/strategy/Client.java
@@ -0,0 +1,15 @@
+package strategy;
+
+public class Client {
+	private final Logging logging;
+	
+	public Client(Logging logging){
+		this.logging = logging;
+	}
+	
+	public void doWork(final int count){
+		if(count %2 == 0){
+			logging.write("Even number : " + count);
+		}
+	}
+}
---
+++ src/main/java/strategy/ConsoleLogging.java
@@ -0,0 +1,8 @@
+package strategy;
+
+public class ConsoleLogging implements Logging{
+	@Override
+	public void write(final String message){
+		System.out.println(message);
+	}
+}
