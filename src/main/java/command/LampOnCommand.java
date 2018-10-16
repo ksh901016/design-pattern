c8cecc79f778594dba916660bcf6d147a24aec1b
sunghyunkang <ksh901016@naver.com>
2018-10-16 21:07:18 +0900

    커맨드패턴

---
+++ src/main/java/command/LampOnCommand.java
@@ -0,0 +1,15 @@
+package command;
+
+public class LampOnCommand implements Command{
+	private Lamp theLamp;
+	
+	public LampOnCommand(Lamp theLamp){
+		this.theLamp = theLamp;
+	}
+	
+	@Override
+	public void execute() {
+		theLamp.turnOn();
+	}
+	
+}
